9a0fee2119152aaa30b16e97deca6aeb9645f61a
rhit-salakbw <salakbw@rose-hulman.edu>
2022-06-10 23:17:47 -0500

    testing RGB LED

---
+++ followAlong/learningC/RGBJoystick/RGBJoystick.ino
@@ -0,0 +1,51 @@
+// C++ code
+//
+int rPot;
+int bPot;
+
+void setup()
+{
+  Serial.begin(9600); //com speed. bits per second
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  
+  //pinMode(2, INPUT_PULLUP);
+  
+}
+
+void loop()
+{
+  red();
+  delay(1000);
+  green();
+  delay(1000);
+  off();
+  delay(1000);
+}
+
+void rgb(int r, int g, int b) {
+  // parameters should be 0 to 255
+  analogWrite(9, r);
+  analogWrite(10, g);
+  analogWrite(11, b);
+  
+}
+
+void red() {
+  analogWrite(9, 255);
+  analogWrite(10, 0);
+  analogWrite(11, 0);
+}
+
+void green() {
+  analogWrite(9, 0);
+  analogWrite(10, 255);
+  analogWrite(11, 0);
+}
+
+void off() {
+  analogWrite(9, 0);
+  analogWrite(10, 0);
+  analogWrite(11, 0);
+}
